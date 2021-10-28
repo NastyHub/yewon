5452da58b091d1ce78213fad0b98a410eecb0575
NastyHub <koroperator@gmail.com>
2021-10-29 00:28:36 +0900

    structure done, need a fancy design

--- cogs/money.py
+++ cogs/money.py
@@ -1,10 +1,6 @@
 import discord
-import os
-import json
 from discord.ext import commands, tasks
-import time
 import asyncio
-import random
 import sqlite3
 
 from discord.utils import MAX_ASYNCIO_SECONDS
@@ -30,6 +26,15 @@ ownerid = 631441731350691850
 ##########################################################################
 
 #USEFUL FUNCTIONS
+
+def getbanklist():
+    wow = c.execute("""SELECT bankname FROM banklist WHERE auth_key = 123""").fetchall()
+    banknamelist = []
+    for i in wow:
+        banknamelist.append(i[0])
+    return banknamelist
+
+
 ##########################################################################
 
 class money(commands.Cog):
@@ -40,7 +45,30 @@ class money(commands.Cog):
 
     @client.command(aliases=["계좌생성"])
     async def makeaccount(self, ctx):
-        await ctx.send("ok.")
+        banknamelist = getbanklist()
+
+        gethowmany = len(banknamelist)
+
+        result = "은행종류:"
+        upcomingnumber = 1
+
+        for i in banknamelist:
+            result += f"\n{upcomingnumber}: {i}"
+            upcomingnumber += 1
+        
+        emojilist = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "❌"]
+
+        message = await ctx.reply(result)
+        i = 0
+
+        while i <= gethowmany-1:
+            await message.add_reaction(emojilist[i])
+            i += 1
+        await message.add_reaction(emojilist[5])
+
+    @client.command(aliases=["은행정보"])
+    async def bankinfo(self, ctx, bankname=None):
+        print(",")
 
 def setup(client):
     client.add_cog(money(client))

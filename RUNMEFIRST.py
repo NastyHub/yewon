79bed85e774fefae9e2ff6d8796abe7772a9e35e
NastyHub <koroperator@gmail.com>
2021-10-29 00:11:43 +0900

    IS THIS IT?

--- RUNMEFIRST.py
+++ RUNMEFIRST.py
@@ -8,16 +8,16 @@ conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
 #혹시 초기화 하려면 아랫줄 샾(#) 지워주세요
-c.execute("""DROP TABLE playermoney""")
-c.execute("""DROP TABLE banklist""")
-c.execute("""DROP TABLE blackmarket""")
+#c.execute("""DROP TABLE playermoney""")
+#c.execute("""DROP TABLE banklist""")
+#c.execute("""DROP TABLE blackmarket""")
 
 c.execute("""CREATE TABLE playermoney(userid INTEGAR, nickname TEXT, wallet INTEGAR, blackmoney INTEGAR, bank INTEGAR)""")
-c.execute("""CREATE TABLE banklist(bankname TEXT, bankid INTEGAR, interest INTEGAR, totalmoney INTEGAR, conversionrate INTEGAR, auth_key INTEGAR)""")
+c.execute("""CREATE TABLE banklist(bankname TEXT, bankid INTEGAR, ownerid INTEGAR, interest INTEGAR, totalmoney INTEGAR, conversionrate INTEGAR, auth_key INTEGAR)""")
 c.execute("""CREATE TABLE blackmarket(fee INTEGAR, moneyleft INTEGAR, auth_key INTEGAR)""")
 
-c.execute("""INSERT INTO banklist VALUES("찬우은행", 1, 0, 0, 0, 123)""")
-c.execute("""INSERT INTO banklist VALUES("국민은행", 2, 0, 0, 0, 123)""")
+c.execute("""INSERT INTO banklist VALUES("찬우은행", 1, 0, 631441731350691850, 0, 0, 123)""")
+c.execute("""INSERT INTO banklist VALUES("국민은행", 2, 0, 0, 0, 0, 123)""")
 c.execute("""INSERT INTO blackmarket VALUES(0, 0, 123)""")
 
 
